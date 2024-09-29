0d545c16c53bdd50a2edf86c1be1947ebc48e834
MD889 <demianukmisha22@gmail.com>
2024-09-29 18:04:47 +0300

    Add files via upload

---
+++ Money.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Money
+{
+	public int dollars;
+	public int cents;
+	public Money(int input1, int input2)
+	{
+		dollars = input1;
+		cents = input2;
+		Round();
+	}
+	public void Round()
+	{
+		dollars = dollars + cents / 100;
+		cents = cents - (cents / 100) * 100;
+	}
+	public void OutputDollars()
+	{
+		Console.WriteLine($"You have {dollars} dollars");
+	}
+    public void OutputCents()
+    {
+        Console.WriteLine($"You have {cents} cents");
+    }
+}
