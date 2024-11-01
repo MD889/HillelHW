3d0ac859cc652636a68b45beace3c80431d40f41
MD889 <demianukmisha22@gmail.com>
2024-11-02 01:02:14 +0200

    Add files via upload

---
+++ City.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class City
+{
+    public string Name;
+    public int Population;
+    public City(string name, int population)
+    {
+        Name = name;
+        Population = population;
+    }
+    public static City operator +(City city, int amount)
+    {
+        city.Population += amount;
+        return city;
+    }
+    public static City operator -(City city, int amount)
+    {
+        city.Population -= amount;
+        return city;
+    }
+    public static bool operator ==(City city1, City city2)
+    {
+        if (city1 is null || city2 is null)
+            return false;
+        return city1.Population == city2.Population;
+    }
+    public static bool operator !=(City city1, City city2)
+    {
+        return city1.Population != city2.Population;
+    }
+    public static bool operator <(City city1, City city2)
+    {
+        return city1.Population < city2.Population;
+    }
+    public static bool operator >(City city1, City city2)
+    {
+        return city1.Population > city2.Population;
+    }
+    public override int GetHashCode()
+    {
+        return Population.GetHashCode();
+    }
+    public override bool Equals(object obj)
+    {
+        if (obj is City other)
+        {
+            return this.Population == other.Population;
+        }
+        return false;
+    }
+}
---
+++ CreditCard.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+public class CreditCard
+{
+    public string CardNumber;
+    private int cvc;
+    public int CVC
+    {
+        get { return cvc; }
+        set
+        {
+            if (value < 100 || value > 999)
+                throw new ArgumentException("CVC must consist of three digits");
+            cvc = value;
+        }
+    }
+    private int balance;
+    public int Balance
+    {
+        get { return balance; }
+        set
+        {
+            balance = value;
+        }
+    }
+    public CreditCard(string cardNumber, int cvc, int balance)
+    {
+        CardNumber = cardNumber;
+        CVC = cvc;
+        Balance = balance;
+    }
+    public static CreditCard operator +(CreditCard card, int amount)
+    {
+        card.Balance += amount;
+        return card;
+    }
+    public static CreditCard operator -(CreditCard card, int amount)
+    {
+        card.Balance -= amount;
+        return card;
+    }
+    public static bool operator ==(CreditCard card1, CreditCard card2)
+    {
+        return card1.CVC == card2.CVC;
+    }
+    public static bool operator !=(CreditCard card1, CreditCard card2)
+    {
+        return card1.CVC != card2.CVC;
+    }
+    public static bool operator <(CreditCard card1, CreditCard card2)
+    {
+        return card1.Balance < card2.Balance;
+    }
+    public static bool operator >(CreditCard card1, CreditCard card2)
+    {
+        return card1.Balance > card2.Balance;
+    }
+    public override bool Equals(object obj)
+    {
+        if (obj is CreditCard other)
+        {
+            return this.CVC == other.CVC;
+        }
+        return false;
+    }
+    public override int GetHashCode()
+    {
+        return CVC.GetHashCode();
+    }
+}
