bc02c1a857be13fd8cef7d2be3a4618b3ac3892d
MD889 <demianukmisha22@gmail.com>
2024-11-14 18:19:20 +0200

    Add files via upload

---
+++ Lesson5/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Play play = new Play("Lorem", "ipsum", "dolor", 1875);
+        play.DisplayInfo();
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+
+        Shop shop = new Shop("Lorem", "ipsum", ShopType.Grocery);
+        shop.DisplayInfo();
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+    }
+}
